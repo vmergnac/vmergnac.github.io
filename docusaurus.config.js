1914a9b7767a25d735a8b593591bf8af9687ae96
Vincent Mergnac <vincent.mergnac@unilim.fr>
2022-12-04 22:22:29 +0000

    url ok

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/vmergnac/vmergnac.github.io/blob/main/docs/',
+            'https://github.com/vmergnac/vmergnac.github.io/blob/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/vmergnac/vmergnac.github.io/blob/main/blog/',
+            'https://github.com/vmergnac/vmergnac.github.io/blob/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
